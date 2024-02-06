fd84c87f88fe757f35cab2ff23cbeda0cd74db16
Aprendiz <Aprendiz@BOGDFPCGMFN4155.FormacionSena.RED>
2024-02-06 17:20:21 -0500

    primeras entidades

---
+++ app/entities.py
@@ -0,0 +1,10 @@
+
+class Carro():
+  placa = "SDF 456"
+  tipo_vehiculo = "Camion"
+  
+class Cliente():
+    nombre = "Cristiano Ronaldo"
+    celular = 3142778980
+    identificacion = 52501920
+    
