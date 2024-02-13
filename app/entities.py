e0475ff45f3709447ac6a021aff746cb9533166c
Aprendiz <Aprendiz@BOGDFPCGMFN4163.FormacionSena.RED>
2024-02-13 16:49:14 -0500

    modelo terminado

--- app/entities.py
+++ app/entities.py
@@ -26,6 +26,38 @@ class cliente():
     def listcar(self):
         for i in self.lista_carros:
             print("carro con placas: " + i.placa)
+            
+            
+class cupo():
+    
+    def __init__(self, letra):
+        self.letra = letra
+        
+        
+class pago():
+    
+    def __init__(self,
+                 fecha_inicio,
+                 hora_inicio,
+                 fecha_fin,
+                 hora_fin,
+                 valor,
+                 carro,
+                 cupo,
+                 empleado):
+        
+        self.fecha_inicio = fecha_inicio
+        self.hora_inicio = hora_inicio
+        self.fecha_fin = fecha_fin
+        self.hora_fin = hora_fin
+        self.valor = valor
+        self.carro = carro
+        self.cupo = cupo 
+        self.empleado = empleado   
+                
+        
+    
+    
         
         
         
